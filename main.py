e25703af5b8c2b6b1852c7d0715a598f5d59d951
dload1392 <dload1392@gmail.com>
2021-03-11 21:02:05 +0900

    Comments edited

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ Using SMTP to send an email from Python
 
 Email successfully sent.
 
-
-
 # Regular Expression
 ^로 시작, $로 종료
 +는 1회 이상 반복, {2,3}은 2~3회
@@ -16,11 +14,11 @@ Email successfully sent.
 """
 import smtplib
 from email.message import EmailMessage
-import imghdr       # 이미지의 확장자 판단. python 내장
-import re           # Regular Expression
+import imghdr  # 이미지의 확장자 판단. python 내장
+import re  # Regular Expression
 
 SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 465     # Gmail port num
+SMTP_PORT = 465  # Gmail port num
 
 
 def sendEmail(addr):
@@ -30,6 +28,7 @@ def sendEmail(addr):
         print("정상적으로 메일이 발송되었습니다.")
     else:
         print("유효한 이메일 주소가 아닙니다.")
+
 
 msg = EmailMessage()
 email_subject = input("제목을 입력해주세요. :")
@@ -45,14 +44,14 @@ msg["To"] = email_to
 # email content
 msg.set_content(email_content)
 
-with open("test_image.png", "rb") as image:           # rb: read binary
+with open("test_image.png", "rb") as image:  # rb: read binary
     image_file = image.read()
 
 image_type = imghdr.what('test_image', image_file)
 # print(image_type)
 msg.add_attachment(image_file, maintype='image', subtype=image_type)
 
-smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)      # SMTP(server, port)
+smtp = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)  # SMTP(server, port)
 smtp.login(my_email, "###PASSWORD###")
 
 # Call sendEmail to verify and send an email
